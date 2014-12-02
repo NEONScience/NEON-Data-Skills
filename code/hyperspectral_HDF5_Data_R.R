5e5eb3465abbd604937fbb4354ae6ed24f7cdce4
lwasser <lwasser@neoninc.org>
2014-12-02 15:35:21 -0700

    code updates

--- code/hyperspectral_HDF5_Data_R.R
+++ code/hyperspectral_HDF5_Data_R.R
@@ -6,12 +6,39 @@
 library(raster)
 library(rhdf5)
 
+#make sure you have atleast rhdf5 version 2.10 installed
+packageVersion("rhdf5")
+
+#update rhdf5 if needed.
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("rhdf5")
+
 #specify the path to the H5 file. Notice that HDF5 extension can be either "hdf5" or "h5"
-#f <- '/Users/lwasser/Documents/Conferences/1_DataWorkshop_ESA2014/HDF5File/SJER_140123_chip.h5'
-f <- '/Users/law/Documents/data/SJER_140123_chip.h5'
+f <- '/Users/lwasser/Documents/Conferences/1_DataWorkshop_ESA2014/HDF5File/SJER_140123_chip.h5'
+#f <- '/Users/law/Documents/data/SJER_140123_chip.h5'
 
 #look at the HDF5 file structure 
 h5ls(f,all=T)
 
-#r get spatial info and map info
+#r get spatial info and map info using the h5readAttributes function developed by Ted Hart
 spinfo <- h5readAttributes(f,"spatialInfo")
+
+
+#read in the wavelength information from the Hdf5 file
+wavelengths<- h5read(f,"wavelength",index=list(1:426,1))
+
+#r extract "slices" of data from an HDF5 file (read in only the parts that you need)
+b34<- h5read(f,"Reflectance",index=list(1:477,1:502,34))
+
+#Convert from array to matrix
+b34 <- b34[,,1]
+
+#plot image
+image(b34)
+
+#View range of reflectance values.
+#when a program applies color to each pixel in a raster, it stretches
+hist(b34,breaks=40,col="darkmagenta")
+hist(b34,breaks=40,col="darkmagenta",xlim = c(0, 5000))
+
+hist(b34, breaks=40,col="darkmagenta",xlim = c(5000, 15000),ylim=c(0,100))
