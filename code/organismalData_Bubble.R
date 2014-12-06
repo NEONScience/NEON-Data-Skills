54bc5d2f9c99598ec416d3997ba350fd5d3ff7bd
lwasser <lwasser@neoninc.org>
2014-12-05 17:11:51 -0700

    fixed the config

---
+++ code/organismalData_Bubble.R
@@ -0,0 +1,8 @@
+#viz of data...
+
+library(maps)
+map("state", interior = FALSE)
+map("state", boundary = FALSE, col="gray", add = TRUE)
+
+
+http://flowingdata.com/2010/11/23/how-to-make-bubble-charts/
