4b4257a9c5debef9364bc56568efcc472f5de309
lwasser <lwasser@neoninc.org>
2014-12-06 21:04:15 -0700

    tweaking side bar to give cleaner loo

--- code/organismalData_Bubble.R
+++ code/organismalData_Bubble.R
@@ -6,3 +6,11 @@ map("state", boundary = FALSE, col="gray", add = TRUE)
 
 
 http://flowingdata.com/2010/11/23/how-to-make-bubble-charts/
+  
+  
+crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState2005.tsv", header=TRUE, sep="\t")
+symbols(crime$murder, crime$burglary, circles=crime$population)
+
+radius <- sqrt( crime$population/ pi )
+symbols(crime$murder, crime$burglary, circles=radius)
+symbols(crime$murder, crime$burglary, circles=radius, inches=0.35, fg="white", bg="red", xlab="Murder Rate", ylab="Burglary Rate")
