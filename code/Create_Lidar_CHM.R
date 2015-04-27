0b2590a6afee9f1658da17a31d4a731da27468c2
lwasser <lwasser@neoninc.org>
2015-04-27 15:52:27 -0600

    updated the chm code

--- code/Create_Lidar_CHM.R
+++ code/Create_Lidar_CHM.R
@@ -8,20 +8,21 @@ setwd("~/Conferences/1_DataWorkshop_ESA2014/ESAWorkshop_data")
 # ~\Users\law\Documents\data\CHM_InSitu_Data\
 
 # Import DSM into R 
+#please note that the raster package also requires and will load the sp package
 library(raster)
 
 # IMPORTANT - the path to your DSM data may be different than the path below.  
-#dsm_f <- "C:/Users/lwasser/Documents/Conferences/1_DataWorkshop_ESA2014/ESAWorkshop_data/Part3_LiDAR/DigitalSurfaceModel/SJER2013_DSM.tif"
-dsm_f <- "/Users/law/Documents/data/CHM_InSitu_Data/DigitalSurfaceModel/SJER2013_DSM.tif"
+dsm_f <- "C:/Users/lwasser/Documents/workshopData/ESAWorkshop_data/Part3_LiDAR/DigitalSurfaceModel/SJER2013_DSM.tif"
+#dsm_f <- "/Users/law/Documents/data/CHM_InSitu_Data/DigitalSurfaceModel/SJER2013_DSM.tif"
 dsm <- raster(dsm_f)
 ## See info about the raster. notice it has a CRS associated with it.
 dsm
 plot(dsm)
 
 # import the digital terrain model
-#dtm_f <- "C:/Users/lwasser/Documents/Conferences/1_DataWorkshop_ESA2014/ESAWorkshop_data/Part3_LiDAR/DigitalTerrainModel/SJER2013_DTM.tif"
+dtm_f <- "C:/Users/lwasser/Documents/workshopData/ESAWorkshop_data/Part3_LiDAR/DigitalTerrainModel/SJER2013_DTM.tif"
 #mac 
-dtm_f <- "/Users/law/Documents/data/CHM_InSitu_Data/DigitalTerrainModel/SJER2013_DTM.tif"
+#dtm_f <- "/Users/law/Documents/data/CHM_InSitu_Data/DigitalTerrainModel/SJER2013_DTM.tif"
 
 dtm <- raster(dtm_f)
 plot(dtm)
@@ -49,10 +50,17 @@ library(dplyr)
 
 #import the centroid data and the vegetation structure data
 options(stringsAsFactors=FALSE)
-centroids <- read.csv("/Users/law/Documents/data/CHM_InSitu_Data/InSitu_Data/SJERPlotCentroids.csv")
-insitu_dat <- read.csv("/Users/law/Documents/data/CHM_InSitu_Data/InSitu_Data/D17_2013_vegStr.csv")
 
-#Overlay the centroid points and the stem locations to the CHM plot.
+# Please note that the syntax for a path is slightly different between mac and pc.
+#mac
+#centroids <- read.csv("/Users/law/Documents/data/CHM_InSitu_Data/InSitu_Data/SJERPlotCentroids.csv")
+#insitu_dat <- read.csv("/Users/law/Documents/data/CHM_InSitu_Data/InSitu_Data/D17_2013_vegStr.csv")
+
+#pc
+centroids <- read.csv("/Users/lwasser/Documents/workshopData/ESAWorkshop_data/InSitu_Data/SJERPlotCentroids.csv")
+insitu_dat <- read.csv("/Users/lwasser/Documents/workshopData/ESAWorkshop_data/InSitu_Data/D17_2013_vegStr.csv")
+
+#Overlay the centroid points and the stem locations on top of CHM plot.
 points(centroids$easting,centroids$northing, pch=22, cex = 4,col = 2)
 points(insitu_dat$easting,insitu_dat$northing, pch=19, cex=.5)
 
@@ -67,25 +75,39 @@ centroid_sp <- SpatialPoints(centroids[,4:3],proj4string =CRS(as.character(chm@c
 #buffer =- radius, not diameter
 cent_ovr <- extract(chm,centroid_sp,buffer = 20)
 
+
+## VARIATION 2 -- Extract CHM Values using a shapefile
+#to import you need the maptools library which depends upon the rgeos packages.
+#install.packages(rgeos)
+#install.packages(maptools)
+
+
+library(maptools)
+squarePlot <- readShapePoly("/Users/lwasser/Documents/workshopData/ESAWorkshop_data/InSitu_Data/SJERPlotCentroids_Buffer.shp")
+cent_ovr2 <- extract(chm, squarePlot, weights=FALSE, fun=max)
+
+
 #extract the max CHM value for each plot within the 10 m buffer defined above. Add it to the centroids object
 centroids$chmMax <- unlist(lapply(cent_ovr,max))
+
+
 
 #now, there are two ways to do the next part. Let's start with writing each line out.
 
 #first slelect plots that are also represented in our centroid layer. Quick test - how many plots are in the centroid folder?
-insitu_inCentroid <- insitu_dat %.% filter(plotid %in% centroids$Plot_ID)
+insitu_inCentroid <- insitu_dat %>% filter(plotid %in% centroids$Plot_ID)
 
 #list out plot id results. how many are there?
 unique(insitu_inCentroid$plotid) 
 
 
 #find the max stem height value for each plot. We will compare this value to the max CHM value.
-insitu_maxStemHeight <- insitu_inCentroid %.% group_by(plotid) %.% summarise(max = max(stemheight))
+insitu_maxStemHeight <- insitu_inCentroid %>% group_by(plotid) %>% summarise(max = max(stemheight))
 
 
 #NOTE -- we can be super tricky and combine the above steps into one line of code. See below how this is done.
 #this string is taking full advantage of the dplyr package.
-insitu <- insitu_dat %.% filter(plotid %in% centroids$Plot_ID) %.% group_by(plotid) %.% summarise(quant = quantile(stemheight,.95), max = max(stemheight))
+insitu <- insitu_dat %>% filter(plotid %in% centroids$Plot_ID) %>% group_by(plotid) %>% summarise(quant = quantile(stemheight,.95), max = max(stemheight))
 
 #add the max height value to the centroids object.
 centroids$insitu <- insitu_maxStemHeight$max
