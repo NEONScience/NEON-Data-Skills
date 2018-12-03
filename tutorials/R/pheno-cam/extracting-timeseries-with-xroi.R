3d4773e23d142ef87ba5af8d1f54aea413c2f561
bnasr <bijan.s.nasr@gmail.com>
2018-12-03 10:04:19 -0700

    purl xROI

---
+++ tutorials/R/pheno-cam/extracting-timeseries-with-xroi.R
@@ -0,0 +1,32 @@
+## ----install-CRAN, echo=TRUE, eval=FALSE---------------------------------
+## 
+## utils::install.packages('xROI', repos = "http://cran.us.r-project.org" )
+## 
+
+## ----install-GitHub, echo=TRUE, eval=FALSE-------------------------------
+## 
+## # install devtools first
+## utils::install.packages('devtools', repos = "http://cran.us.r-project.org" )
+## 
+## # use devtools to install from GitHub
+## devtools::install_github("bnasr/xROI")
+## 
+
+## ----launch-r, echo=TRUE, eval=FALSE-------------------------------------
+## # load xROI
+## library(xROI)
+## 
+## # launch xROI
+## Launch()
+## 
+
+## 
+## ----launch-xroi, echo=TRUE, eval=FALSE----------------------------------
+## 
+## # launch data in ROI
+## # first edit the path below to the dowloaded directory you just extracted
+## xROI::Launch('/path/to/extracted/directory')
+## 
+## # alternatively, you can run without specifying a path and use the interface to browse
+## 
+
