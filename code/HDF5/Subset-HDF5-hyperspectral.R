5f28a8a0288211210165be4b518cb49c742381c5
Donal-at-NEON <olearyd@battelleecology.org>
2020-03-09 11:26:09 -0600

    Full update for this tutorial in response to Megan's first round of revisions.

--- code/HDF5/Subset-HDF5-hyperspectral.R
+++ code/HDF5/Subset-HDF5-hyperspectral.R
@@ -1,4 +1,4 @@
-## ----load-libraries------------------------------------------------------
+## ----load-libraries--------------------------------------------------------------------
 
 # Install rhdf5 package (only need to run if not already installed)
 # install.packages("BiocManager")
@@ -9,25 +9,29 @@ library(rhdf5)
 
 
 
-## ----read-in-file--------------------------------------------------------
+## ----wd-and-filename-------------------------------------------------------------------
 # set working directory to ensure R can find the file we wish to import and where
 # we want to save our files. Be sure to move the download into your working directory!
-wd="~/Desktop/Hyperspectral_Tutorial/" #This will depend on your local environment
+wd <- "~/Documents/data/" # This will depend on your local environment
 setwd(wd)
 
-# Read in H5 file
+# Make the name of our HDF5 file a variable
 f <- paste0(wd,"NEON_D17_SJER_DP3_257000_4112000_reflectance.h5")
 
 
 
-## ----create-hdf5---------------------------------------------------------
+## ----view-original, eval=FALSE, comment=NA---------------------------------------------
+View(h5ls(f, all=T))
+
+
+## ----create-hdf5-----------------------------------------------------------------------
 # First, create a name for the new file
 nf <- paste0(wd, "NEON_hyperspectral_tutorial_example_subset.h5")
 
-
 # create hdf5 file
-
 h5createFile(nf)
+
+# Now we create the groups that we will use to organize our data
 h5createGroup(nf, "SJER/")
 h5createGroup(nf, "SJER/Reflectance")
 h5createGroup(nf, "SJER/Reflectance/Metadata")
@@ -36,87 +40,145 @@ h5createGroup(nf, "SJER/Reflectance/Metadata/Spectral_Data")
 
 
 
-## ----ref-attributes------------------------------------------------------
+## ----ref-attributes--------------------------------------------------------------------
 
-a=h5readAttributes(f,"/SJER/Reflectance/")
+a <- h5readAttributes(f,"/SJER/Reflectance/")
 fid <- H5Fopen(nf)
-g=H5Gopen(fid, "SJER/Reflectance")
+g <- H5Gopen(fid, "SJER/Reflectance")
 
 for(i in 1:length(names(a))){
-h5writeAttribute(attr = a[[i]], h5obj=g, name=names(a[i]))
+  h5writeAttribute(attr = a[[i]], h5obj=g, name=names(a[i]))
 }
+
+# It's always a good idea to close the file connection immidiately
+# after finishing each step that leaves an open connection.
 h5closeAll()
 
 
-## ----populate-group-attributes-------------------------------------------
+## ----populate-group-attributes---------------------------------------------------------
 
 # make a list of all groups within the full tile file
-ls=h5ls(f,all=T)
+ls <- h5ls(f,all=T)
 
-#make a list of all of the names within the Coordinate_System group
-cg=unique(ls[ls$group=="/SJER/Reflectance/Metadata/Coordinate_System",]$name)
+# make a list of all of the names within the Coordinate_System group
+cg <- unique(ls[ls$group=="/SJER/Reflectance/Metadata/Coordinate_System",]$name)
 
+# Loop through the list of datasets that we just made above
 for(i in 1:length(cg)){
   print(cg[i])
-  a=h5readAttributes(f,paste0("/SJER/Reflectance/Metadata/Coordinate_System/",cg[i]))
+  
+  # Read the inividual dataset within the Coordinate_System group
   d=h5read(f,paste0("/SJER/Reflectance/Metadata/Coordinate_System/",cg[i]))
+
+  # Read the associated attributes (if any)
+  a=h5readAttributes(f,paste0("/SJER/Reflectance/Metadata/Coordinate_System/",cg[i]))
+    
+  # Assign the attributes (if any) to the dataset
   attributes(d)=a
+  
+  # Finally, write the dataset to the HDF5 file
   h5write(obj=d,file=nf,
           name=paste0("/SJER/Reflectance/Metadata/Coordinate_System/",cg[i]),
           write.attributes=T)
 }
 
 
-## ----subset-wavelengths--------------------------------------------------
-#First, we make out 'index'  a list of number that will allow us to select every fourth band
+## ----subset-wavelengths----------------------------------------------------------------
+
+# First, we make our 'index', a list of number that will allow us to select every fourth band, using the "sequence" function seq()
 idx <- seq(from = 1, to = 426, by = 4)
 
 # We then use this index to select particular wavelengths from the full tile using the "index=" argument
-ws <- h5read(file = f, 
+wavelengths <- h5read(file = f, 
              name = "SJER/Reflectance/Metadata/Spectral_Data/Wavelength", 
              index = list(idx)
             )
 
 # As per above, we also need the wavelength attributes
-wa <- h5readAttributes(file = f, 
+wavelength.attributes <- h5readAttributes(file = f, 
                        name = "SJER/Reflectance/Metadata/Spectral_Data/Wavelength")
-attributes(ws) <- wa
+attributes(wavelengths) <- wavelength.attributes
 
 # Finally, write the subset of wavelengths and their attributes to the subset file
-h5write(obj=ws, file=nf,
+h5write(obj=wavelengths, file=nf,
         name="SJER/Reflectance/Metadata/Spectral_Data/Wavelength",
         write.attributes=T)
 
 
-## ----read-hyp-data-------------------------------------------------------
-# Read in reflectance data - note the index argument! This is ultimately how we reduce the file size.
+## ----plot-example-band-----------------------------------------------------------------
+# Extract or "slice" data for band 58 from the HDF5 file
+b58 <- h5read(f,name = "SJER/Reflectance/Reflectance_Data",
+             index=list(58,NULL,NULL))
+h5closeAll()
+
+# convert from array to matrix
+b58 <- b58[1,,]
+
+# Make a plot to view this band
+image(log(b58), col=grey(0:100/100))
+
+
+
+## ----plot-example-band-subset----------------------------------------------------------
+subset_rows <- 1:500
+subset_columns <- 501:1000
+# Extract or "slice" data for band 44 from the HDF5 file
+b58 <- h5read(f,name = "SJER/Reflectance/Reflectance_Data",
+             index=list(58,subset_columns,subset_rows))
+h5closeAll()
+
+# convert from array to matrix
+b58 <- b58[1,,]
+
+# Make a plot to view this band
+image(log(b58), col=grey(0:100/100))
+
+
+
+## ----read-hyp-data---------------------------------------------------------------------
+
+# Read in reflectance data.
+# Note the list that we feed into the index argument! 
+# This tells the h5read() function which bands, rows, and 
+# columns to read. This is ultimately how we reduce the file size.
 hs <- h5read(file = f, 
              name = "SJER/Reflectance/Reflectance_Data", 
-             index = list(idx, 501:1000, 1:500)
+             index = list(idx, subset_columns, subset_rows)
             )
-# grab the '$dim' attribute - as this will be needed when writing the file at the bottom
-hsd=attributes(hs)
 
-# We also need the attributes for the reflectance data, of course.
+
+## ----hyp-data-attributes---------------------------------------------------------------
+
+# grab the '$dim' attribute - as this will be needed 
+# when writing the file at the bottom
+hsd <- attributes(hs)
+
+# We also need the attributes for the reflectance data.
 ha <- h5readAttributes(file = f, 
                        name = "SJER/Reflectance/Reflectance_Data")
 
-# However, some of the attributes are not longer valid since we changed the spatial extend of this dataset.
-# therefore, we will need to overwrite those with the correct values.
-ha$Dimensions=c(500,500,107)
+# However, some of the attributes are not longer valid since 
+# we changed the spatial extend of this dataset. therefore, 
+# we will need to overwrite those with the correct values.
+ha$Dimensions=c(500,500,107) # Note that the HDF5 file saves dimensions in a different order than R reads them
 ha$Spatial_Extent_meters[1]=ha$Spatial_Extent_meters[1]+500
 ha$Spatial_Extent_meters[3]=ha$Spatial_Extent_meters[3]+500
 attributes(hs)=c(hsd,ha)
 
+# View the combined attributes to ensure they are correct
 attributes(hs)
 
+# Finally, write the hyperspectral data, plus attributes, 
+# to our new file 'nf'.
 h5write(obj=hs, file=nf,
         name="SJER/Reflectance/Reflectance_Data",
         write.attributes=T)
 
+# It's always a good idea to close the HDF5 file connection
+# before moving on.
 h5closeAll()
 
 
-## ----view-product, eval=FALSE, comment=NA--------------------------------
+## ----view-product, eval=FALSE, comment=NA----------------------------------------------
 View(h5ls(nf, all=T))
 
