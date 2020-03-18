86417f001c1ef475989247c2f4632ab8c0f81658
donal-at-NEON <olearyd@battelleecology.org>
2020-03-17 18:33:31 -0600

    Made a new script to determine automatically which .md files do not have an associated .Rmd file - these are the ones that will need to be changed in a separate batch from the .Rmd files. The .md files that do have an associated .Rmd file will be re-written by the .Rmd upon knitting.

---
+++ processing_code/Redesign-find-lone-md-files.R
@@ -0,0 +1,26 @@
+## script to evaluate how many .md files do not have an associated .Rmd file
+
+# Inputs - Where the git repo is on your computer
+#gitRepoPath <-"~/Git/dev-aten/NEON-Data-Skills"
+
+#gitRepoPath <- path.expand(gitRepoPath) # expand tilde to later remove this root dir from longer filepaths
+
+
+# find all .Rmd files
+rmd.files <- list.files("~/Git/dev-aten/NEON-Data-Skills/old-tutorials/tutorials",
+                        pattern="\\.Rmd$", full.names = TRUE, recursive = TRUE)
+# Find all .md files
+md.files <- list.files("~/Git/dev-aten/NEON-Data-Skills/old-tutorials/tutorials",
+                        pattern="\\.md$", full.names = TRUE, recursive = TRUE)
+
+# Remove any .md files from the list if they contain "/Python/" in their filepath
+# https://stackoverflow.com/questions/40885360/remove-entries-from-string-vector-containing-specific-characters-in-r/40885527
+md.files <- md.files[!grepl(paste0("/Python/", collapse = "|"), md.files)]
+
+rmd.files[1]
+md.files[14]
+
+rmd.files <- gsub("\\.Rmd","",rmd.files)
+md.files <- gsub("\\.md","",md.files)
+
+unpaired.md <- md.files[!md.files %in% rmd.files]
