6e04850e00731ceef3e81502a4232594bbb9ef65
donal-at-NEON <olearyd@battelleecology.org>
2020-03-19 12:20:37 -0600

    Updating change URL script to make changes for all .Rmd and .md files. Added some annotation as well.

--- processing_code/Redesign_scripts/Redesign_replace_graphics_URLs.R
+++ processing_code/Redesign_scripts/Redesign_replace_graphics_URLs.R
@@ -1,6 +1,5 @@
 # Replace graphics links with static URLs 
-# Use gsub() to find instances of {{ site.baseurl }} AND {{site.baseurl}} that end with an image suffix (png, gif, jpg, any others??)
-# Also lookfor {{ site.baseurl}} and {{site.baseurl }} ?? Maybe some others will pop up too
+# Use gsub() to find instances of {{ site.baseurl }} AND {{site.baseurl}}
 
 master_dir <- "~/Git/dev-aten/NEON-Data-Skills"
 
@@ -8,26 +7,32 @@ master_dir <- "~/Git/dev-aten/NEON-Data-Skills"
 # Do this again for .md files!
 Rmd.files <- list.files("~/Git/dev-aten/NEON-Data-Skills/tutorials",
                         pattern="\\.Rmd$", full.names = TRUE, recursive = TRUE)
+md.files <- list.files("~/Git/dev-aten/NEON-Data-Skills/tutorials",
+                        pattern="\\.md$", full.names = TRUE, recursive = TRUE)
 
+all.files=c(Rmd.files, md.files)
 
+# character strings to find and their replacements.
+# There may or may not be spaces surrounding "site.baseurl"
 
-# file path to image file folder
-# this should not need to change for different tutorials
+# First, replace the image references
 img.base <- "{{ site.baseurl }}/images/"
 img.base2 <- "{{site.baseurl}}/images/"
 rep_graphics_url <- "https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/dev-aten/graphics/"
 
+# Next, replace neonscience.org baseurl references
 neon.base <- "{{ site.baseurl }}/"
 neon.base2 <- "{{site.baseurl}}/"
 rep_neon_url <- "https://www.neonscience.org/"
 
+# Finally, replace relative URL pathway references
 rel.base <- "href=\"/"
 rep_rel_url <- "href=\"https://www.neonscience.org/"
-# replce this with rep_neon_url
 
-file=Rmd.files[26]
+# Test a single file
+#file=Rmd.files[26]
 
-#for (file in Rmd.files){
+for (file in all.files){
 
 # open .md (or .Rmd) file
 fileConn <- file(file)
@@ -49,7 +54,4 @@ fl.mod <- gsub(rel.base, rep_rel_url, fl.mod, fixed=TRUE)
 writeLines(fl.mod, fileConn)
 close(fileConn)      
 
-  
-  
-  
-# } #END file
+} #END file
