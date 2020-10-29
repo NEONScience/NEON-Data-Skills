caffca7e7d27e2874261733f84749c67b43e4c64
donal-at-NEON <olearyd@battelleecology.org>
2020-10-29 11:46:04 -0600

    Posting Python API #1 to preview page

---
+++ processing_code/Redesign_scripts/Redesign_process_ipynb_files.R
@@ -0,0 +1,25 @@
+## convert python files to .md, .py, .html files and make figures
+
+rm(list=ls())
+
+input.path="~/Git/dev-aten/NEON-Data-Skills/tutorials/Python/NEON-API-python"
+
+# Find all files to change
+ipynb.files <- list.files(input.path,
+                        pattern="\\.ipynb$", full.names = T, recursive = TRUE)
+ipynb.files
+
+basename(ipynb.files)
+dirname(ipynb.files)
+
+for(p in 1:length(ipynb.files)){
+  
+  print(paste("file #",p,ipynb.files[p]))
+  system(paste0("cd ",dirname(ipynb.files[p]),
+                "; jupyter nbconvert --to html ",basename(ipynb.files[p]),
+                "; jupyter nbconvert --to script ",basename(ipynb.files[p]),
+                "; jupyter nbconvert --to markdown ",basename(ipynb.files[p])))
+  system("mkdir test2")
+  system("pwd")
+  
+}
