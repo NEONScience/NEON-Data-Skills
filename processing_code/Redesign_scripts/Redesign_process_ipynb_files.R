8012e6366ce16f28cba490b957711411f2fd597a
donal-at-NEON <olearyd@battelleecology.org>
2020-11-27 08:03:51 -0700

    Updating graphics URLs in Python-based tutorials to point to 'main' instead of 'dev-aten'

--- processing_code/Redesign_scripts/Redesign_process_ipynb_files.R
+++ processing_code/Redesign_scripts/Redesign_process_ipynb_files.R
@@ -2,22 +2,30 @@
 
 rm(list=ls())
 
-input.path="~/Git/dev-aten/NEON-Data-Skills/tutorials/Python"
+input.path="~/Git/main/NEON-Data-Skills/tutorials/Python"
 
 # Find all files to change
 ipynb.files <- list.files(input.path,
                         pattern="\\.ipynb$", full.names = T, recursive = TRUE)
 ipynb.files
+ipynb.files <- ipynb.files[7]
 
 basename(ipynb.files)
 dirname(ipynb.files)
 
+
+
+
 for(p in 1:length(ipynb.files)){
   
   print(paste("file #",p,ipynb.files[p]))
+  
+  # system(paste0("cd ",dirname(ipynb.files[p]),
+  # "; jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ",basename(ipynb.files[p])))
+    
   system(paste0("cd ",dirname(ipynb.files[p]),
                 "; jupyter nbconvert --to html ",basename(ipynb.files[p]),
                 "; jupyter nbconvert --to script ",basename(ipynb.files[p]),
                 "; jupyter nbconvert --to markdown ",basename(ipynb.files[p])))
-  
+
 }
