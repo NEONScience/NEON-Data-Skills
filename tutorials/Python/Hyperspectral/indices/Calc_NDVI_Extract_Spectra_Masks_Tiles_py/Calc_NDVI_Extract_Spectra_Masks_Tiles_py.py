d6f237f8a5a51a424061ca16c3526cd324f0e0f1
donal-at-NEON <olearyd@battelleecology.org>
2020-11-27 10:02:44 -0700

    Uploading test products of updated python processing_code - veryfying that code1 works, expecting working 'graphics' and broken 'figures' - will need to re-point .md file links to figures produces in code

--- tutorials/Python/Hyperspectral/indices/Calc_NDVI_Extract_Spectra_Masks_Tiles_py/Calc_NDVI_Extract_Spectra_Masks_Tiles_py.py
+++ tutorials/Python/Hyperspectral/indices/Calc_NDVI_Extract_Spectra_Masks_Tiles_py/Calc_NDVI_Extract_Spectra_Masks_Tiles_py.py
@@ -1,83 +1,88 @@
----
-syncID: 19e0b890b3c64f46b2189c8273a2e0a4
-title: "Calculate NDVI & Extract Spectra Using Masks in Python - Tiled Data"
-description: "Learn to calculate Normalized Difference Vegetation Index (NDVI) and extract spectral using masks with Python and NEON tiled hyperspectral data products." 
-dateCreated: 2018-07-05 
-authors: Bridget Hass
-contributors: 
-estimatedTime: 
-packagesLibraries: numpy, h5py, gdal, matplotlib.pyplot
-topics: hyperspectral-remote-sensing, HDF5, remote-sensing, 
-languagesTool: python
-dataProduct: NEON.DP3.30006, NEON.DP3.30008
-code1: https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/main/tutorials/Python/Hyperspectral/indices/Calc_NDVI_Extract_Spectra_Masks_Tiles_py/Calc_NDVI_Extract_Spectra_Masks_Tiles_py.ipynb
-tutorialSeries: intro-hsi-py-series
-urlTitle: calc-ndvi-tiles-py
----
+#!/usr/bin/env python
+# coding: utf-8
 
-In this tutorial, we will calculate the Normalized Difference Vegetation Index 
-(NDVI). 
+# ---
+# syncID: 19e0b890b3c64f46b2189c8273a2e0a4
+# title: "Calculate NDVI & Extract Spectra Using Masks in Python - Tiled Data"
+# description: "Learn to calculate Normalized Difference Vegetation Index (NDVI) and extract spectral using masks with Python and NEON tiled hyperspectral data products." 
+# dateCreated: 2018-07-05 
+# authors: Bridget Hass
+# contributors: 
+# estimatedTime: 
+# packagesLibraries: numpy, h5py, gdal, matplotlib.pyplot
+# topics: hyperspectral-remote-sensing, HDF5, remote-sensing, 
+# languagesTool: python
+# dataProduct: NEON.DP3.30006, NEON.DP3.30008
+# code1: https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/main/tutorials/Python/Hyperspectral/indices/Calc_NDVI_Extract_Spectra_Masks_Tiles_py/Calc_NDVI_Extract_Spectra_Masks_Tiles_py.ipynb
+# tutorialSeries: intro-hsi-py-series
+# urlTitle: calc-ndvi-tiles-py
+# ---
 
-This tutorial uses the mosaiced or tiled NEON data product. For a tutorial 
-using the flightline data, please see <a href="/calc-ndvi-py" target="_blank"> Calculate NDVI & Extract Spectra Using Masks in Python - Flightline Data</a>.
+# In this tutorial, we will calculate the Normalized Difference Vegetation Index 
+# (NDVI). 
+# 
+# This tutorial uses the mosaiced or tiled NEON data product. For a tutorial 
+# using the flightline data, please see <a href="/calc-ndvi-py" target="_blank"> Calculate NDVI & Extract Spectra Using Masks in Python - Flightline Data</a>.
+# 
+# 
+# <div id="ds-objectives" markdown="1">
+# 
+# ### Objectives
+# After completing this tutorial, you will be able to:
+# 
+# * Calculate NDVI from hyperspectral data in Python.
+# 
+# ### Install Python Packages
+# 
+# * **numpy**
+# * **pandas**
+# * **gdal** 
+# * **matplotlib** 
+# * **h5py**
+# 
+# 
+# ### Download Data
+# 
+# {% include/dataSubsets/_data_DI18.html %}
+# 
+# [[nid:7489]]
+# 
+# </div>
+# 
+
+# # Calculate NDVI & Extract Spectra with Masks
+# 
+# ## Background:
+# The Normalized Difference Vegetation Index (NDVI) is a standard band-ratio calculation frequently used to analyze ecological remote sensing data. NDVI indicates whether the remotely-sensed target contains live green vegetation. When sunlight strikes objects, certain wavelengths of the electromagnetic spectrum are absorbed and other wavelengths are reflected. The pigment chlorophyll in plant leaves strongly absorbs visible light (with wavelengths in the range of 400-700 nm) for use in photosynthesis. The cell structure of the leaves, however, strongly reflects near-infrared light (wavelengths ranging from 700 - 1100 nm). Plants reflect up to 60% more light in the near infrared portion of the spectrum than they do in the green portion of the spectrum. By calculating the ratio of Near Infrared (NIR) to Visible (VIS) bands in hyperspectral data, we can obtain a metric of vegetation density and health.  
+# 
+# The formula for NDVI is: $$NDVI = \frac{(NIR - VIS)}{(NIR+ VIS)}$$
+# 
+# <figure>
+# 	<a href="https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/main/graphics/hyperspectral-indices/ndvi_tree.png">
+# 	<img src="https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/main/graphics/hyperspectral-indices/ndvi_tree.png"></a>
+# 	<figcaption> NDVI is calculated from the visible and near-infrared light
+#     reflected by vegetation. Healthy vegetation (left) absorbs most of the
+#     visible light that hits it, and reflects a large portion of
+#     near-infrared light. Unhealthy or sparse vegetation (right) reflects more
+#     visible light and less near-infrared light. Source: <a href="https://www.researchgate.net/figure/266947355_fig1_Figure-1-Green-vegetation-left-absorbs-visible-light-and-reflects-near-infrared-light" target="_blank">Figure 1 in Wu et. al. 2014. PLOS. </a>   
+# 	</figcaption>
+# </figure>
+# 
+# Start by setting plot preferences and loading the neon_aop_refl_hdf5_functions module:
+
+# In[1]:
 
 
-<div id="ds-objectives" markdown="1">
-
-### Objectives
-After completing this tutorial, you will be able to:
-
-* Calculate NDVI from hyperspectral data in Python.
-
-### Install Python Packages
-
-* **numpy**
-* **pandas**
-* **gdal** 
-* **matplotlib** 
-* **h5py**
-
-
-### Download Data
-
-{% include/dataSubsets/_data_DI18.html %}
-
-[[nid:7489]]
-
-</div>
-
-
-# Calculate NDVI & Extract Spectra with Masks
-
-## Background:
-The Normalized Difference Vegetation Index (NDVI) is a standard band-ratio calculation frequently used to analyze ecological remote sensing data. NDVI indicates whether the remotely-sensed target contains live green vegetation. When sunlight strikes objects, certain wavelengths of the electromagnetic spectrum are absorbed and other wavelengths are reflected. The pigment chlorophyll in plant leaves strongly absorbs visible light (with wavelengths in the range of 400-700 nm) for use in photosynthesis. The cell structure of the leaves, however, strongly reflects near-infrared light (wavelengths ranging from 700 - 1100 nm). Plants reflect up to 60% more light in the near infrared portion of the spectrum than they do in the green portion of the spectrum. By calculating the ratio of Near Infrared (NIR) to Visible (VIS) bands in hyperspectral data, we can obtain a metric of vegetation density and health.  
-
-The formula for NDVI is: $$NDVI = \frac{(NIR - VIS)}{(NIR+ VIS)}$$
-
-<figure>
-	<a href="https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/main/graphics/hyperspectral-indices/ndvi_tree.png">
-	<img src="https://raw.githubusercontent.com/NEONScience/NEON-Data-Skills/main/graphics/hyperspectral-indices/ndvi_tree.png"></a>
-	<figcaption> NDVI is calculated from the visible and near-infrared light
-    reflected by vegetation. Healthy vegetation (left) absorbs most of the
-    visible light that hits it, and reflects a large portion of
-    near-infrared light. Unhealthy or sparse vegetation (right) reflects more
-    visible light and less near-infrared light. Source: <a href="https://www.researchgate.net/figure/266947355_fig1_Figure-1-Green-vegetation-left-absorbs-visible-light-and-reflects-near-infrared-light" target="_blank">Figure 1 in Wu et. al. 2014. PLOS. </a>   
-	</figcaption>
-</figure>
-
-Start by setting plot preferences and loading the neon_aop_refl_hdf5_functions module:
-
-
-```python
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline 
+get_ipython().run_line_magic('matplotlib', 'inline')
 import warnings
 warnings.filterwarnings('ignore') #don't display warnings
-```
 
 
-```python
+# In[2]:
+
+
 # %load ../neon_aop_hyperspectral.py
 """
 Created on Wed Jun 20 10:34:49 2018
@@ -235,98 +240,86 @@ def plot_aop_rgb(rgbArray,ext,ls_pct=5,plot_title=''):
     plt.title(plot_title + '\n Linear ' + str(ls_pct) + '% Contrast Stretch'); 
     ax = plt.gca(); ax.ticklabel_format(useOffset=False, style='plain') #do not use scientific notation #
     rotatexlabels = plt.setp(ax.get_xticklabels(),rotation=90) #rotate x tick labels 90 degree
-```
 
 
-![png](Calc_NDVI_Extract_Spectra_Masks_Tiles_py_files/Calc_NDVI_Extract_Spectra_Masks_Tiles_py_4_0.png)
+# ### Read in SERC Reflectance Tile
+
+# In[3]:
 
 
-### Read in SERC Reflectance Tile
-
-
-```python
 sercRefl, sercRefl_md = aop_h5refl2array('/Users/olearyd/Git/data/NEON_D02_SERC_DP3_368000_4306000_reflectance.h5')
-```
-
-### Extract NIR and VIS bands
-
-Now that we have uploaded all the required functions, we can calculate NDVI and plot it. 
-Below we print the center wavelengths that these bands correspond to:
 
 
-```python
+# ### Extract NIR and VIS bands
+# 
+# Now that we have uploaded all the required functions, we can calculate NDVI and plot it. 
+# Below we print the center wavelengths that these bands correspond to:
+
+# In[4]:
+
+
 print('band 58 center wavelength (nm): ',sercRefl_md['wavelength'][57])
 print('band 90 center wavelength (nm) : ', sercRefl_md['wavelength'][89])
-```
-
-    band 58 center wavelength (nm):  668.985
-    band 90 center wavelength (nm) :  829.238
 
 
-## Calculate & Plot NDVI
+# ## Calculate & Plot NDVI
 
-Here we see that band 58 represents red visible light, while band 90 is in the NIR portion of the spectrum. Let's extract these two bands from the reflectance array and calculate the ratio using the `numpy.divide` which divides arrays element-wise. 
+# Here we see that band 58 represents red visible light, while band 90 is in the NIR portion of the spectrum. Let's extract these two bands from the reflectance array and calculate the ratio using the `numpy.divide` which divides arrays element-wise. 
+
+# In[5]:
 
 
-```python
 vis = sercRefl[:,:,57]
 nir = sercRefl[:,:,89]
 
 ndvi = np.divide((nir-vis),(nir+vis))
-```
-
-We can use the function `plot_aop_refl` to plot this, and choose the `seismic` color pallette to highlight the difference between positive and negative NDVI values. Since this is a normalized index, the values should range from -1 to +1. 
 
 
-```python
+# We can use the function `plot_aop_refl` to plot this, and choose the `seismic` color pallette to highlight the difference between positive and negative NDVI values. Since this is a normalized index, the values should range from -1 to +1. 
+
+# In[6]:
+
+
 plot_aop_refl(ndvi,sercRefl_md['spatial extent'],
               colorlimit = (np.min(ndvi),np.max(ndvi)),
               title='SERC Subset NDVI \n (VIS = Band 58, NIR = Band 90)',
               cmap_title='NDVI',
               colormap='seismic')
-```
 
 
-![png](Calc_NDVI_Extract_Spectra_Masks_Tiles_py_files/Calc_NDVI_Extract_Spectra_Masks_Tiles_py_13_0.png)
+# ## Extract Spectra Using Masks
+# 
+# In the second part of this tutorial, we will learn how to extract the average spectra of pixels whose NDVI exceeds a specified threshold value. There are several ways to do this using `numpy`, including the mask functions `numpy.ma`, as well as `numpy.where` and finally using `boolean` indexing. 
+# 
+# To start, lets copy the NDVI calculated above and use booleans to create an array only containing NDVI > 0.6.
+
+# In[7]:
 
 
-## Extract Spectra Using Masks
-
-In the second part of this tutorial, we will learn how to extract the average spectra of pixels whose NDVI exceeds a specified threshold value. There are several ways to do this using `numpy`, including the mask functions `numpy.ma`, as well as `numpy.where` and finally using `boolean` indexing. 
-
-To start, lets copy the NDVI calculated above and use booleans to create an array only containing NDVI > 0.6.
-
-
-```python
 import copy
 ndvi_gtpt6 = copy.copy(ndvi)
 #set all pixels with NDVI < 0.6 to nan, keeping only values > 0.6
 ndvi_gtpt6[ndvi<0.6] = np.nan  
 print('Mean NDVI > 0.6:',round(np.nanmean(ndvi_gtpt6),2))
-```
-
-    Mean NDVI > 0.6: 0.87
 
 
+# In[8]:
 
-```python
+
 plot_aop_refl(ndvi_gtpt6,
                 sercRefl_md['spatial extent'],
                 colorlimit=(0.6,1),
                 title='SERC Subset NDVI > 0.6 \n (VIS = Band 58, NIR = Band 90)',
                 cmap_title='NDVI',
                 colormap='RdYlGn')
-```
 
 
-![png](Calc_NDVI_Extract_Spectra_Masks_Tiles_py_files/Calc_NDVI_Extract_Spectra_Masks_Tiles_py_16_0.png)
+# ## Calculate the mean spectra, thresholded by NDVI 
+# Below we will demonstrate how to calculate statistics on arrays where you have applied a mask `numpy.ma`. In this example, the function calculates the mean spectra for values that remain after masking out values by a specified threshold. 
+
+# In[9]:
 
 
-## Calculate the mean spectra, thresholded by NDVI 
-Below we will demonstrate how to calculate statistics on arrays where you have applied a mask `numpy.ma`. In this example, the function calculates the mean spectra for values that remain after masking out values by a specified threshold. 
-
-
-```python
 import numpy.ma as ma
 def calculate_mean_masked_spectra(reflArray,ndvi,ndvi_threshold,ineq='>'):
     
@@ -346,20 +339,22 @@ def calculate_mean_masked_spectra(reflArray,ndvi,ndvi_threshold,ineq='>'):
         mean_masked_refl[i] = ma.mean(masked_refl)
             
     return mean_masked_refl
-```
-
-We can test out this function for various NDVI thresholds. We'll test two together, and you can try out different values on your own. Let's look at the average spectra for healthy vegetation (NDVI > 0.6), and for a lower threshold (NDVI < 0.3).
 
 
-```python
+# We can test out this function for various NDVI thresholds. We'll test two together, and you can try out different values on your own. Let's look at the average spectra for healthy vegetation (NDVI > 0.6), and for a lower threshold (NDVI < 0.3).
+
+# In[10]:
+
+
 sercSpectra_ndvi_gtpt6 = calculate_mean_masked_spectra(sercRefl,ndvi,0.6)
 sercSpectra_ndvi_ltpt3 = calculate_mean_masked_spectra(sercRefl,ndvi,0.3,ineq='<') 
-```
-
-Finally, we can use `pandas` to plot the mean spectra. First set up the pandas dataframe.
 
 
-```python
+# Finally, we can use `pandas` to plot the mean spectra. First set up the pandas dataframe.
+
+# In[11]:
+
+
 import pandas
 
 #Remove water vapor bad band windows & last 10 bands 
@@ -377,12 +372,13 @@ sercSpectra_ndvi_df = pandas.DataFrame()
 sercSpectra_ndvi_df['wavelength'] = w
 sercSpectra_ndvi_df['mean_refl_ndvi_gtpt6'] = sercSpectra_ndvi_gtpt6
 sercSpectra_ndvi_df['mean_refl_ndvi_ltpt3'] = sercSpectra_ndvi_ltpt3
-```
-
-Plot the masked NDVI dataframe to display the mean spectra for NDVI values that exceed 0.6 and that are less than 0.3:
 
 
-```python
+# Plot the masked NDVI dataframe to display the mean spectra for NDVI values that exceed 0.6 and that are less than 0.3:
+
+# In[12]:
+
+
 ax = plt.gca();
 sercSpectra_ndvi_df.plot(ax=ax,x='wavelength',y='mean_refl_ndvi_gtpt6',color='green',
                          edgecolor='none',kind='scatter',label='NDVI > 0.6',legend=True);
@@ -392,8 +388,4 @@ ax.set_title('Mean Spectra of Reflectance Masked by NDVI')
 ax.set_xlim([np.nanmin(w),np.nanmax(w)]); ax.set_ylim(0,0.45)
 ax.set_xlabel("Wavelength, nm"); ax.set_ylabel("Reflectance")
 ax.grid('on'); 
-```
-
-
-![png](Calc_NDVI_Extract_Spectra_Masks_Tiles_py_files/Calc_NDVI_Extract_Spectra_Masks_Tiles_py_24_0.png)
 
