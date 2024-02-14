7f10d737d44b8463229c3b5d94ecc471d7c39181
Claire Lunch <clunch@neoninc.org>
2024-02-14 08:47:45 -0700

    adjusting release v prov tutorial

--- tutorials/R/NEON-general/neon-overview/release-provisional/release-provisional.R
+++ tutorials/R/NEON-general/neon-overview/release-provisional/release-provisional.R
@@ -60,15 +60,58 @@ gg
 
 
 
-## ----plot-all-quality------------------------------------------------------------------------------
+## ----load-release-2023, results="hide"-------------------------------------------------------------
 
-gg <- ggplot(qpr$PARQL_30min[which(qpr$PARQL_30min$horizontalPosition=="001"),], 
+qpr23 <- loadByProduct(dpID="DP1.00066.001", 
+                     site=c("HEAL", "GUAN"),
+                     startdate="2021-01",
+                     enddate="2021-12",
+                     release="RELEASE-2023",
+                     check.size=F)
+
+
+
+## ----load-release-2024, results="hide"-------------------------------------------------------------
+
+qpr24 <- loadByProduct(dpID="DP1.00066.001", 
+                     site=c("HEAL", "GUAN"),
+                     startdate="2021-01",
+                     enddate="2021-12",
+                     release="RELEASE-2024",
+                     check.size=F)
+
+
+
+## ----plot-release-compare--------------------------------------------------------------------------
+
+gg <- ggplot(qpr23$PARQL_30min
+             [which(qpr23$PARQL_30min$horizontalPosition=="001"),], 
              aes(endDateTime, linePARMean)) +
   geom_line() +
-  facet_wrap(~siteID) + 
-  geom_rug(data=qpr$PARQL_30min[which(qpr$PARQL_30min$horizontalPosition=="001" 
-                                      & qpr$PARQL_30min$finalQF==1),], 
-             color="red", sides="b")
+  facet_wrap(~siteID) +
+  geom_line(data=qpr24$PARQL_30min
+            [which(qpr24$PARQL_30min$horizontalPosition=="001"),], 
+            color="blue", alpha=0.3)
+gg
+
+
+
+## ----get-issues------------------------------------------------------------------------------------
+
+tail(qpr24$issueLog_00066)
+
+
+
+## ----plot-release-compare-k------------------------------------------------------------------------
+
+gg <- ggplot(qpr23$PARQL_30min
+             [which(qpr23$PARQL_30min$horizontalPosition=="001"),], 
+             aes(endDateTime, linePARKurtosis)) +
+  geom_line() +
+  facet_wrap(~siteID) +
+  geom_line(data=qpr24$PARQL_30min
+            [which(qpr24$PARQL_30min$horizontalPosition=="001"),], 
+            color="blue", alpha=0.3)
 gg
 
 
