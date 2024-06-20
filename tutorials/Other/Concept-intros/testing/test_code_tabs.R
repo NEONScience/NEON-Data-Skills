7b7cd790b1292e3124af9ad35b3136eece2de285
Claire Lunch <clunch@neoninc.org>
2024-06-20 08:34:11 -0600

    revert to full html doc

--- tutorials/Other/Concept-intros/testing/test_code_tabs.R
+++ tutorials/Other/Concept-intros/testing/test_code_tabs.R
@@ -6,6 +6,6 @@ library(htmltools)
 
 ## ----html---------------------------------------------------------------------------------------------------------------
 
-htmltools::includeHTML("https://github.com/NEONScience/NEON-Data-Skills/raw/main/tutorials-in-development/Other/test_code_tabs.html")
+htmltools::includeHTML("https://github.com/cklunch/NEON-Data-Skills/blob/main/tutorials-in-development/Other/test_code_tabs.html")
 
 
