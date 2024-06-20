032df552a33652a87a4545f6021889cb35038cad
Claire Lunch <clunch@neoninc.org>
2024-06-20 08:38:38 -0600

    test iframe

--- tutorials/Other/Concept-intros/testing/test_code_tabs.R
+++ tutorials/Other/Concept-intros/testing/test_code_tabs.R
@@ -6,6 +6,6 @@ library(htmltools)
 
 ## ----html---------------------------------------------------------------------------------------------------------------
 
-htmltools::includeHTML("https://github.com/cklunch/NEON-Data-Skills/blob/main/tutorials-in-development/Other/test_code_tabs.html")
+htmltools::tags$iframe(src="https://github.com/cklunch/NEON-Data-Skills/blob/main/tutorials-in-development/Other/test_code_tabs.html", width="100%", height=1600)
 
 
