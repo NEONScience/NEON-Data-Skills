b5676595a9c874b2fb9a409865c617ab28098366
Claire Lunch <clunch@neoninc.org>
2024-06-24 15:18:44 -0600

    iframe option

--- tutorials/Other/Concept-intros/testing/test_code_tabs.R
+++ tutorials/Other/Concept-intros/testing/test_code_tabs.R
@@ -2,7 +2,7 @@
 
 library(htmltools)
 library(httr)
-res <- GET("https://api.github.com/repos/NEONScience/NEON-Data-Skills/contents/tutorials-in-development/Other/test_code_tabs.html")
+res <- GET("https://api.github.com/repos/cklunch/NEON-Data-Skills/contents/tutorials-in-development/Other/test_code_tabs.html")
 r2 <- content(res)
 download.file(r2$download_url, destfile=paste(getwd(), "/tut_html.html", sep=""), mode="wb", quiet=T)
 
@@ -10,7 +10,8 @@ download.file(r2$download_url, destfile=paste(getwd(), "/tut_html.html", sep="")
 
 ## ----html, echo=FALSE, warning=FALSE, message=FALSE--------------------------------------------------------------------
 
-htmltools::tags$embed(paste(getwd(), "/tut_html.html", sep=""))
+htmltools::tags$iframe(src = base64enc::dataURI(file=paste(getwd(), "/tut_html.html", sep=""), 
+                                                mime="text/html; charset=UTF-8"), width="100%", height=1600)
 
 
 
