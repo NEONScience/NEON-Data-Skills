f4d15537220ef6c486aa3ed0d8c853c63fdb2831
Claire Lunch <clunch@neoninc.org>
2024-06-25 15:45:11 -0600

    3 html options

---
+++ tutorials/Other/Concept-intros/test_tabs_full_doc/test_code_tabs_document.R
@@ -0,0 +1,11 @@
+## ----setup, echo=FALSE---------------------------------------------------------------------------------------
+
+library(htmltools)
+
+
+
+## ----html, echo=FALSE, warning=FALSE, message=FALSE----------------------------------------------------------
+
+htmltools::includeHTML("https://github.com/cklunch/NEON-Data-Skills/blob/main/tutorials-in-development/Other/test_code_tabs.html")
+
+
---
+++ tutorials/Other/Concept-intros/test_tabs_iframe/test_code_tabs_iframe.R
@@ -0,0 +1,23 @@
+## ----setup, echo=FALSE---------------------------------------------------------------------------------------
+
+library(htmltools)
+library(httr)
+res <- GET("https://api.github.com/repos/cklunch/NEON-Data-Skills/contents/tutorials-in-development/Other/test_code_tabs.html")
+r2 <- content(res)
+download.file(r2$download_url, destfile=paste(getwd(), "/tut_html.html", sep=""), mode="wb", quiet=T)
+
+
+
+## ----html, echo=FALSE, warning=FALSE, message=FALSE----------------------------------------------------------
+
+htmltools::tags$iframe(src = base64enc::dataURI(file=paste(getwd(), "/tut_html.html", sep=""), 
+                                                mime="text/html; charset=UTF-8"), width="100%", 
+                       height=1600, frameborder="0", seamless="seamless")
+
+
+
+## ----cleanup, echo=FALSE-------------------------------------------------------------------------------------
+
+unlink(paste(getwd(), "/tut_html.html", sep=""))
+
+
