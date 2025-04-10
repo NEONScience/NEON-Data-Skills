7dd8ee6517bc378fa2a75b224660acc801c983b4
bhass-neon <bhass@battelleecology.org>
2025-04-10 16:04:58 -0600

    minor updates

--- tutorials/R/AOP/Lidar/lidar-uncertainty/lidar_raster_uncertainty.R
+++ tutorials/R/AOP/Lidar/lidar-uncertainty/lidar_raster_uncertainty.R
@@ -1,4 +1,4 @@
-## ----load-libraries, message=FALSE, warning=FALSE---------------------------------------------------------------------
+## ----load-libraries, message=FALSE, warning=FALSE--------------------------------------------------------------------
 
 # Load needed packages
 library(terra)
@@ -6,13 +6,13 @@ library(neonUtilities)
 library(ggplot2)
 
 
-## ----set-working-directory--------------------------------------------------------------------------------------------
+## ----set-working-directory-------------------------------------------------------------------------------------------
 
 wd="~/data/" #This will depend on your local environment
 setwd(wd)
 
 
-## ----download-chm, eval=FALSE-----------------------------------------------------------------------------------------
+## ----download-chm, eval=FALSE----------------------------------------------------------------------------------------
 ## byTileAOP(dpID='DP3.30015.001',
 ##           site='PRIN',
 ##           year='2016',
@@ -22,7 +22,7 @@ setwd(wd)
 ##           savepath = wd)
 
 
-## ----download-dem, eval=FALSE-----------------------------------------------------------------------------------------
+## ----download-dem, eval=FALSE----------------------------------------------------------------------------------------
 ## byTileAOP(dpID='DP3.30024.001',
 ##           site='PRIN',
 ##           year='2016',
@@ -32,7 +32,7 @@ setwd(wd)
 ##           savepath = wd)
 
 
-## ----define-lidar-paths, results="hide"-------------------------------------------------------------------------------
+## ----define-lidar-paths, results="hide"------------------------------------------------------------------------------
 # Define the CHM, DSM and DTM file names, including the full path
 chm_file1 <- paste0(wd,"DP3.30015.001/neon-aop-products/2016/FullSite/D11/2016_PRIN_1/L3/DiscreteLidar/CanopyHeightModelGtif/NEON_D11_PRIN_DP3_607000_3696000_CHM.tif")
 chm_file2 <- paste0(wd,"DP3.30015.001/neon-aop-products/2016/FullSite/D11/2016_PRIN_2/L3/DiscreteLidar/CanopyHeightModelGtif/NEON_D11_PRIN_DP3_607000_3696000_CHM.tif")
@@ -42,7 +42,7 @@ dtm_file1 <- paste0(wd,"DP3.30024.001/neon-aop-products/2016/FullSite/D11/2016_P
 dtm_file2 <- paste0(wd,"DP3.30024.001/neon-aop-products/2016/FullSite/D11/2016_PRIN_2/L3/DiscreteLidar/DTMGtif/NEON_D11_PRIN_DP3_607000_3696000_DTM.tif")
 
 
-## ----terra-rasters----------------------------------------------------------------------------------------------------
+## ----terra-rasters---------------------------------------------------------------------------------------------------
 
 # assign raster to object
 chm1 <- rast(chm_file1)
@@ -56,7 +56,7 @@ dtm2 <- rast(dtm_file2)
 chm1
 
 
-## ----plot-dsm---------------------------------------------------------------------------------------------------------
+## ----plot-dsm--------------------------------------------------------------------------------------------------------
 # plot the set of DTM rasters
 # Set up the plotting area to have 1 row and 2 columns
 par(mfrow = c(1, 2))
@@ -66,7 +66,7 @@ plot(dsm1, main = "2016_PRIN_1 DSM")
 plot(dsm2, main = "2016_PRIN_2 DSM")
 
 
-## ----plot-dtm---------------------------------------------------------------------------------------------------------
+## ----plot-dtm--------------------------------------------------------------------------------------------------------
 # Reset the plotting area
 par(mfrow = c(1, 2))
 
@@ -75,7 +75,7 @@ plot(dtm1, main = "2016_PRIN_1 DTM")
 plot(dtm2, main = "2016_PRIN_2 DTM")
 
 
-## ----diff-dsm---------------------------------------------------------------------------------------------------------
+## ----diff-dsm--------------------------------------------------------------------------------------------------------
 
 # Difference the 2 DSM rasters
 dsm_diff <- dsm1 - dsm2
@@ -90,7 +90,7 @@ print(paste("Standard Deviation of DSM Difference:", round(dsm_diff_std_dev,3)))
 
 
 
-## ----dsm-diff-hist----------------------------------------------------------------------------------------------------
+## ----dsm-diff-hist---------------------------------------------------------------------------------------------------
 
 # Set options to avoid scientific notation
 options(scipen = 999)
@@ -100,7 +100,7 @@ hist(dsm_diff, breaks = 100, main = "Histogram of DSM Difference", xlab = "Heigh
 
 
 
-## ----dsm-diff-hist-zoomed-in------------------------------------------------------------------------------------------
+## ----dsm-diff-hist-zoomed-in-----------------------------------------------------------------------------------------
 
 # Calculate x-axis limits: mean ± 2 * standard deviation
 xlim_lower <- dsm_diff_mean - 2 * dsm_diff_std_dev
@@ -113,7 +113,7 @@ hist(dsm_diff, breaks = 250, xlim = c(xlim_lower, xlim_upper),
 
 
 
-## ----spatial-distribution-dsm-diff------------------------------------------------------------------------------------
+## ----spatial-distribution-dsm-diff-----------------------------------------------------------------------------------
 
 custom_palette <- colorRampPalette(c("blue", "white", "red"))(9)
 breaks = c(-1.75, -1.25, -0.75, -0.25, 0.25, 0.75, 1.25, 2)
@@ -128,7 +128,7 @@ plot(dsm_diff,
 
 
 
-## ----diff-dtm---------------------------------------------------------------------------------------------------------
+## ----diff-dtm--------------------------------------------------------------------------------------------------------
 
 # Difference the 2 DTM rasters
 dtm_diff <- dtm1 - dtm2
@@ -143,7 +143,7 @@ print(paste("Standard Deviation of DSM Difference:", round(dtm_diff_std_dev,3)))
 
 
 
-## ----spatial-distribution-dtm-diff------------------------------------------------------------------------------------
+## ----spatial-distribution-dtm-diff-----------------------------------------------------------------------------------
 
 custom_palette <- colorRampPalette(c("blue", "white", "red"))(5)
 breaks = c(-1, -0.6, -0.2, 0.2, 0.6, 1)
@@ -156,7 +156,7 @@ plot(dtm_diff,
      legend = TRUE)
 
 
-## ----plot-chm---------------------------------------------------------------------------------------------------------
+## ----plot-chm--------------------------------------------------------------------------------------------------------
 # Reset the plotting area
 par(mfrow = c(1, 2))
 
@@ -165,7 +165,7 @@ plot(chm1, main = "2016_PRIN_1 CHM")
 plot(chm2, main = "2016_PRIN_2 CHM")
 
 
-## ----mask-dsm-veg-pixels----------------------------------------------------------------------------------------------
+## ----mask-dsm-veg-pixels---------------------------------------------------------------------------------------------
 
 # Create a mask of non-zero values in the CHM raster
 chm_mask <- chm1 != 0
@@ -183,7 +183,7 @@ print(paste("Standard Deviation of DSM Difference on Veg Pixels:", round(dsm_dif
 
 
 
-## ----masked-dsm-histogram---------------------------------------------------------------------------------------------
+## ----masked-dsm-histogram--------------------------------------------------------------------------------------------
 
 # Calculate x-axis limits: mean ± 2 * standard deviation
 masked_xlim_lower <- dsm_diff_masked_veg_mean - 2 * dsm_diff_masked_veg_std_dev
@@ -195,7 +195,7 @@ hist(dsm_diff_masked_veg, breaks = 250, xlim = c(masked_xlim_lower, masked_xlim_
      ylab = "Frequency", col = "lightblue", border = "black")
 
 
-## ----mask-dtm-veg-pixels----------------------------------------------------------------------------------------------
+## ----mask-dtm-veg-pixels---------------------------------------------------------------------------------------------
 
 # Apply the veg mask to the DTM difference raster
 dtm_diff_masked_veg <- mask(dtm_diff, chm_mask, maskvalue = FALSE)
@@ -209,7 +209,7 @@ print(paste("Mean DTM Difference on Veg Pixels:", round(dtm_diff_masked_veg_mean
 print(paste("Standard Deviation of DTM Difference on Veg Pixels:", round(dtm_diff_masked_veg_std_dev,3)))
 
 
-## ----masked-dtm-histogram---------------------------------------------------------------------------------------------
+## ----masked-dtm-histogram--------------------------------------------------------------------------------------------
 
 # Calculate x-axis limits: mean ± 2 * standard deviation
 masked_xlim_lower <- dtm_diff_masked_veg_mean - 2 * dtm_diff_masked_veg_std_dev
@@ -221,7 +221,7 @@ hist(dtm_diff_masked_veg, breaks = 250, xlim = c(masked_xlim_lower, masked_xlim_
      ylab = "Frequency", col = "lightblue", border = "black")
 
 
-## ----mask-dsm-ground--------------------------------------------------------------------------------------------------
+## ----mask-dsm-ground-------------------------------------------------------------------------------------------------
 
 ground_mask <- chm1 == 0
 
@@ -237,7 +237,7 @@ print(paste("Mean DSM Difference on Ground Pixels:", round(dsm_diff_masked_groun
 print(paste("Standard Deviation of DSM Difference on Ground Pixels:", round(dsm_diff_masked_ground_std_dev,3)))
 
 
-## ----dsm-diff-masked-ground-hist--------------------------------------------------------------------------------------
+## ----dsm-diff-masked-ground-hist-------------------------------------------------------------------------------------
 
 # Calculate x-axis limits: mean ± 2 * standard deviation
 masked_xlim_lower <- dsm_diff_masked_ground_mean - 2 * dsm_diff_masked_ground_std_dev
