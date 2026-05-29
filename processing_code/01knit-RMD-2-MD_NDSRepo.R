db8bc7efe480d68f31dac5a307d877fe3e8d3f12
Claire Lunch <clunch@neoninc.org>
2026-05-29 13:15:54 -0600

    knit for token update

--- processing_code/01knit-RMD-2-MD_NDSRepo.R
+++ processing_code/01knit-RMD-2-MD_NDSRepo.R
@@ -30,7 +30,7 @@ dirs <- c("Tabbed/NEON-general/NEON-download-explore",
 
 # set directory (order above) that you'd like to build
 
-subDir <- dirs[4]
+subDir <- dirs[5]
 
 # Inputs - Where the git repo is on your computer
 gitRepoPath <-"~/GitHub/NEON-Data-Skills"
